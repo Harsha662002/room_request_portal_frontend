33e5252a8e3097bca7a8f9e700d290b31fcd42a8
bhendi-boi <bhendiisgreen@gmail.com>
2023-07-10 13:57:10 +0530

    appending colors and fonts rather than overwriting them

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,16 +12,16 @@ module.exports = {
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
-    },
-    fontFamily: {
-      inter: ["Inter", "sans-serif"],
-    },
-    colors: {
-      // club_prevrequests
-      "club-previous-requests-header-text": "#000000",
-      "dark-club-previous-requests-header-text": "#fdfdfd",
-      "club_prevrequests-card-bg": "#D9D9D9",
-      "club_prevrequests-card-text": "#222222",
+      fontFamily: {
+        inter: ["Inter", "sans-serif"],
+      },
+      colors: {
+        // club_prevrequests
+        "club-previous-requests-header-text": "#000000",
+        "dark-club-previous-requests-header-text": "#fdfdfd",
+        "club_prevrequests-card-bg": "#D9D9D9",
+        "club_prevrequests-card-text": "#222222",
+      },
     },
   },
   plugins: [],
